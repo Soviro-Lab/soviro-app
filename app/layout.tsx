ff67168dddac75f425f2a95ef01ae24134a6eeaf
JohnMalvin12 <85529444+JohnMalvin12@users.noreply.github.com>
2025-02-26 13:00:00 +0000

    Add magic UI components for home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,6 @@ export default function RootLayout({
   children,
   children: React.ReactNode;
 }) {
-  return (
     <html lang="en">
       <body className={inter.className}>
         {children}
